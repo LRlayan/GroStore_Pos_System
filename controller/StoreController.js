2a0e8e93406e9c9bec992656597f75daf39301b9
LRLayan <rameshlayan4@gmail.com>
2024-05-07 00:57:39 +0530

    add item details in table

---
+++ controller/StoreController.js
@@ -0,0 +1,26 @@
+import Store from "../model/Store.js";
+import {store} from "../db/DB.js"
+
+$('#submitStore').on('click' , ()=>{
+    let itemCode = $('#itemCode').val();
+    let itemName = $('#itemName').val();
+    let QTYOnHand = $('#inputQTY').val();
+    let unitPrice = $('#unitPrice').val();
+
+    let storeDetail = new Store(itemCode,itemName,QTYOnHand,unitPrice)
+    store.push(storeDetail)
+    loadTable()
+})
+
+function loadTable(){
+    $('#storeTable').empty()
+    store.map(function (storeDetails){
+        let record = `<tr>
+                                  <td class="s-code orderTableBody">${storeDetails.itemCode}</td>  
+                                  <td class="s-name orderTableBody">${storeDetails.itemName}</td>  
+                                  <td class="s-qty orderTableBody">${storeDetails.QTYOnHand}</td>  
+                                  <td class="s-price orderTableBody">${storeDetails.unitPrice}</td>  
+                             </tr>`
+        $('#storeTable').append(record)
+    })
+}
