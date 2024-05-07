ed8de7a4aac20e623bbc0e795002a1f9b2d3fa71
LRLayan <rameshlayan4@gmail.com>
2024-05-07 17:23:28 +0530

    set data input field after select code

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -13,3 +13,16 @@ import {orders,store,customer} from "../db/DB.js"
             }
         })
     });
+
+$('#selectItemCode').change(function() {
+    // Get the selected value using val()
+    var selectedValue = $(this).val();
+
+    store.map(function (store){
+        if (selectedValue === store.itemCode){
+            $('#itemNameP').val(store.itemName)
+            $('#qtyOnHandP').val(store.QTYOnHand)
+            $('#inputPriceP').val(store.unitPrice)
+        }
+    })
+});
